2ac68d3087ceb1fbf5878497765656a7cf6a0051
slawwwik88-alt <slawwwik88@gmail.com>
2026-09-19 12:36:07 +0300

    Add traced Android demo UI

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("com.android.application")
+}
+android {
+    namespace = "com.example.oblikdemo"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.example.oblikdemo"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 7
+        versionName = "0.7-demo"
+    }
+}
