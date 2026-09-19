35ab1b7fe0163af4bfb2adff98a6d4c3bcfff2c6
slawwwik88-alt <slawwwik88@gmail.com>
2026-09-19 20:11:57 +0300

    Build v12 demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
         applicationId = "com.example.oblikdemo"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.4-demo"
+        versionCode = 16
+        versionName = "1.5-demo"
     }
 }
